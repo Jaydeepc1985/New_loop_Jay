ce950ac7e8a9c0f26225f08d383e4ab71c34b380
Your Name <you@example.com>
2025-08-10 17:30:55 +0530

    code push

--- Dev/main.tf
+++ Dev/main.tf
@@ -8,46 +8,46 @@ module "abc" {
   cnt        = var.cnt_x
   depends_on = [module.xyz]
 }
-module "flexi_server" {
-  source       = "../flexi server"
-  flexi_server = var.flexi_server_x
-  depends_on   = [module.xyz, module.VM]
-}
-module "Vnet" {
-  source     = "../vnet"
-  vnet_y     = var.vnet_x
-  depends_on = [module.xyz]
+# module "flexi_server" {
+#   source       = "../flexi server"
+#   flexi_server = var.flexi_server_x
+#   depends_on   = [module.xyz, module.VM]
+# }
+# module "Vnet" {
+#   source     = "../vnet"
+#   vnet_y     = var.vnet_x
+#   depends_on = [module.xyz]
 
-}
-module "subnet" {
-  source     = "../subnet"
-  sub_front  = var.sub_front_x
-  depends_on = [module.Vnet]
+# }
+# module "subnet" {
+#   source     = "../subnet"
+#   sub_front  = var.sub_front_x
+#   depends_on = [module.Vnet]
   
-}
-module "public_ip" {
-  source     = "../publicip"
-  public_ip  = var.public_ip_x
-  depends_on = [module.Vnet]
+# }
+# module "public_ip" {
+#   source     = "../publicip"
+#   public_ip  = var.public_ip_x
+#   depends_on = [module.Vnet]
 
-}
-module "nic" {
-  source       = "../nic"
-  nic_config   = var.nic_config_x
-  depends_on   = [module.subnet, module.public_ip]
-}
-module "VM" {
-  source      = "../VM"
-  frontend_vm = var.frontend_vm_x
-  backend_vm  = var.backend_vm_x
-  depends_on  = [module.nic, module.kv_admin_roles]
+# }
+# module "nic" {
+#   source       = "../nic"
+#   nic_config   = var.nic_config_x
+#   depends_on   = [module.subnet, module.public_ip]
+# }
+# module "VM" {
+#   source      = "../VM"
+#   frontend_vm = var.frontend_vm_x
+#   backend_vm  = var.backend_vm_x
+#   depends_on  = [module.nic, module.kv_admin_roles]
 
-}
-module "NSG" {
-  source     = "../NSG" 
-  NSG        = var.NSG_x
-  depends_on = [module.nic, module.VM]
-}
+# }
+# module "NSG" {
+#   source     = "../NSG" 
+#   NSG        = var.NSG_x
+#   depends_on = [module.nic, module.VM]
+# }
 
 # module "sqlsrv" {
 #   source     = "../sqlsrv"
@@ -60,17 +60,17 @@ module "NSG" {
 #   depends_on   = [module.sqlsrv]
 # }
 
-module "key_vaults" {
-  source                   = "../key-vaults"
-  key_vaults               = var.key_vaults_x
-  depends_on               = [module.xyz]
-}
+# module "key_vaults" {
+#   source                   = "../key-vaults"
+#   key_vaults               = var.key_vaults_x
+#   depends_on               = [module.xyz]
+# }
 
-module "kv_admin_roles" {
-  source                   = "../keyvault-role"
-  kv_admin_roles           = var.kv_admin_roles_x
-  depends_on               = [module.key_vaults]
-}
+# module "kv_admin_roles" {
+#   source                   = "../keyvault-role"
+#   kv_admin_roles           = var.kv_admin_roles_x
+#   depends_on               = [module.key_vaults]
+# }
 # module "dnszone" {
 #   source                   = "../private_DNS_zone"
 #   dnszone                  = var.dnszone_x
